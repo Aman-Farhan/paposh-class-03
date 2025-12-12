308b2108853823e065d4e929d07589d640f55a74
Aman <amanfarhan2008@gmail.com>
2025-12-12 22:42:53 +0500

    modify

--- script.js
+++ script.js
@@ -6,6 +6,15 @@
 
 
 // post Increament :
+// let num1 = 4;
+// let num2 = num1++;
+// console.log(num1);
+
+
+
+// post decrement
+
+
 let num1 = 4;
-let num2 = num1++;
+let num2 = num1--;
 console.log(num1);
