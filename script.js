e3bd7f5705d12c43c64d92ce4c71ecf6cfcd6783
Aman <amanfarhan2008@gmail.com>
2025-12-12 22:41:34 +0500

    updated

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-let num1 = +prompt();
-let num2 = +prompt();
-// let num3 = 4;
+// let num1 = +prompt();
+// let num2 = +prompt();
+// // let num3 = 4;
 
-alert(num1 + ++num2);
+// alert(num1 + ++num2);
 
+
+// post Increament :
+let num1 = 4;
+let num2 = num1++;
+console.log(num1);
